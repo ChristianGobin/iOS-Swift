17bb42babd45e3b331d722155c09aaf096388657
Christian G <chrisgobin94@gmail.com>
2021-09-22 21:23:12 -0400

    Enum + Classes + Structs practice

--- classesSwift.playground/Contents.swift
+++ classesSwift.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+// Needs to be initialized with default values
 class Warrior {
     // Properties and Methods
     var name : String = ""
@@ -18,3 +18,45 @@ myCharacter.strength = 9001
 myCharacter.name = "ErhShang"
 
 myCharacter.charge()
+
+
+// Structs do not need to be initialized with default values
+struct Bed {
+    var legs : Int
+    var size : String
+}
+
+var myBed = Bed(legs : 4, size : "King")
+
+// Enumerators
+
+enum colors {
+    case red
+    case yellow
+    case blue
+    case blank
+}
+
+var savedColor = colors.blue
+print(savedColor)
+
+
+// Adding an enum to a class
+
+class SmartPhone {
+    var brand : String = ""
+    var screenSize : Int = 0
+    var operatingSystem : String = ""
+    var color : colors = .blank
+    
+    func printDesc(){
+        print("Made by \(brand). Operation System : \(operatingSystem)")
+    }
+    
+}
+
+var iphoneXR = SmartPhone()
+iphoneXR.brand = "Apple"
+iphoneXR.operatingSystem = "iOS 14"
+print(iphoneXR.color)
+iphoneXR.printDesc()
