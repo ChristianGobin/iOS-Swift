7827de03b8918095418bba8981c1f73e49a03a80
Christian G <chrisgobin94@gmail.com>
2021-10-04 22:39:44 -0400

    Create scrapWork.swift

---
+++ scrapWork.swift
@@ -0,0 +1,11 @@
+var topThreeMovies = ["GoodFellas", "Godfather", "Oh Hello on Broadway"]
+for movie in topThreeMovies {
+	print(movie)
+}
+
+// Tuples and sets
+
+var myNames = ("Christian", "Hari", "Larry")
+print(myNames.2)
+
+// Sets
