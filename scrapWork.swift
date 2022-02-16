86654666234dbf8c9cfaf27d3d7975f30831cfc1
Christian G <chrisgobin94@gmail.com>
2022-02-15 21:44:09 -0500

    Update scrapWork.swift
    More Practice

--- scrapWork.swift
+++ scrapWork.swift
@@ -1,16 +1,41 @@
+import Cocoa
 import Foundation
 
-// Array declaration with type specified
-var myItems : [String] = ["Christian", "Orlanys"]
+//Arrays
+var nameList : [String] = ["Christian", "Cameron", "Zack"];
 
-// Passing array as argument to function
-func printsEachElement(nums : [String]){
-	
-	// For Each Loop used.
-	for item in nums {
-		print(item)
+//Functions + If-Else Statements.
+func isGreaterThan92(num : Int) -> Bool {
+	if(num <= 92){
+		return false;
 	}
+	return true;
 }
 
-// Called function and passed array to function
-printsEachElement(nums: myItems)
+print(isGreaterThan92(num: 92));
+print(isGreaterThan92(num: 100));
+
+
+//For Each
+for name in nameList {
+	print(name);
+}
+
+//While Loops
+var count : Int = 0;
+while count < 10 {
+	print(count);
+	count += 2;
+}
+
+func findEvenOnly(numArray : [Int]) -> [Int] {
+	var answerArray : [Int] = [];
+	for num in numArray {
+		if num % 2 == 0 {
+			answerArray.append(num);
+		}
+	}
+	return answerArray;
+}
+var randomShit : [Int] = [291, 321, 43, 22, 12, 6, 10, 20];
+print(findEvenOnly(numArray: randomShit));
